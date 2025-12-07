882ea8608a160bdda72c97dcb30b7126dffaff26
FlenderDavid <103044088+FlenderDavid@users.noreply.github.com>
2025-12-07 14:56:48 +0100

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'login', 'register', 'cars','logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:5173'],
+    'allowed_origins' => ['http://127.0.0.1:5173','http://localhost:5173'],
     'allowed_headers' => ['*'], 
     'exposed_headers' => [],
     'max_age' => 0,
